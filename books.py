3bd4753c87b022a8c9deaa133451c4dff6650fa3
Recep Dogan <rdogan8891@gmail.com>
2023-09-20 15:12:27 +0200

    book is selected by id

--- books.py
+++ books.py
@@ -3,14 +3,20 @@ from fastapi import FastAPI
 app= FastAPI()
 
 BOOKS = [
-    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
-    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
-    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
-    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
-    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
-    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+    {'id':'1','title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'id':'2','title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'id':'3','title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'id':'4','title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'id':'5','title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'id':'6','title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
 ]
 
-@app.get("/api-endpoint")
-async def first_api():
-    return {"message": "Hello Recep"}
+@app.get("/books")
+async def getBooks():
+    return BOOKS
+
+@app.get("/books/{id}")
+async def get_book(id: str):
+    for book in BOOKS:
+        if id == book['id']:
+            return book
