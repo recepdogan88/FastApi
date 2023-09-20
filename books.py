21cac0af80edc12a9a65ced89803245604119918
Recep Dogan <rdogan8891@gmail.com>
2023-09-20 16:10:08 +0200

    book_title endpoint has been created

--- books.py
+++ books.py
@@ -8,14 +8,22 @@ BOOKS = [
     {'id':'3','title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
     {'id':'4','title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
     {'id':'5','title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
-    {'id':'6','title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+    {'id':'6','title': 'Title Six', 'author': 'Author Six', 'category': 'math'}
 ]
 
 @app.get("/books")
 async def getBooks():
     return BOOKS
 
-@app.get("/books/{id}")
+
+
+@app.get("/books/{book_title}")
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+        
+@app.get("/books/id/{id}")
 async def get_book(id: str):
     for book in BOOKS:
         if id == book['id']:
